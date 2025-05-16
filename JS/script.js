929d750864fb545ac96837c8db49d778df69c778
patrick <patmcibass@gmail.com>
2025-05-16 16:39:41 -0500

    css

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,22 @@ fetch ("./JS/data.json")
         obj.forEach((e) => createDiv(e))
     });
 
+const menuSection = document.querySelector('.foodMenu')
 
 const createDiv = (obj) => {
     console.log(obj)
+    
+    menuSection.innerHTML+= `
+    <section class="menuItem">
+      <img class="foodImg" src="${obj.image.desktop}" alt="${obj.name}">
+      <button class="addToCart"><img src="./assets/images/icon-add-to-cart.svg" alt="">Add to Cart</button>
+      <section class="description">
+        <p class="category">${obj.category}</p>
+        <h3 class="foodNames">${obj.name}</h3>
+        <p class="price">$${Number(obj.price).toFixed(2)}</p>
+      </section>
+    </section>
+    `
 }
+
+
